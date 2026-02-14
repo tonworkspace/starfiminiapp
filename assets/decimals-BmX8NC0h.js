efc5f1657a8281a1123f37f7d7ea5974c36ca400
tonworkspace <tonstake591@gmail.com>
2026-02-14 02:26:25 +0100

    Updates

---
+++ assets/decimals-BmX8NC0h.js
@@ -0,0 +1,1 @@
+import{w as t}from"./index-c8P_KnTz.js";import{r as a}from"./index-Dodx5EVK.js";const r="0x313ce567",e=[],n=[{type:"uint8"}];async function o(c){return a({contract:c.contract,method:[r,e,n],params:[]})}async function i(c){return t(()=>o(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
