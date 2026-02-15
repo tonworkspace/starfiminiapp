efed15b2da726d44fba440bf97a1f78b7b332b9d
tonworkspace <tonstake591@gmail.com>
2026-02-15 01:03:32 +0100

    Updates

--- assets/decimals-CwjSamEm.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./index-B9LNuLq6.js";import{r as a}from"./index-BEk64rDm.js";const r="0x313ce567",e=[],n=[{type:"uint8"}];async function o(c){return a({contract:c.contract,method:[r,e,n],params:[]})}async function i(c){return t(()=>o(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
--- assets/eth_estimateGas-B81jCIez.js
+++
@@ -1,1 +0,0 @@
-import{t as s}from"./index-B9LNuLq6.js";async function m(t,e){const a=await t({method:"eth_estimateGas",params:[e]});return s(a)}export{m as eth_estimateGas};
